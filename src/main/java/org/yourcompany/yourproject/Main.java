7e503f26d48da81d78a5cf9b42491c45c97e1109
Sofia Vergara <ana.vergara@estudiantesunibague.edu.co>
2026-02-10 17:50:09 -0500

    Implementacion de socio

--- src/main/java/org/yourcompany/yourproject/Main.java
+++ src/main/java/org/yourcompany/yourproject/Main.java
@@ -8,10 +8,13 @@ package org.yourcompany.yourproject;
  *
  * @author anaso
  */
-public class Repaso {
+public class Main {
     public static void main (String[]args){
         Bibliotecario bibliotecario = new Bibliotecario("1", "pepe","pepe@gmail.com","Mañana");
         System.out.println(bibliotecario.darEmail());
+
+        Socio socio = new Socio("2","Juan","juan@gmail.com",5);
+        socio.registrarPrestamo(2);
     }
 
 }
