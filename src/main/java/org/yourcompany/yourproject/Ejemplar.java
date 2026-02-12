8b4895ebe66930366aba5435e2f5428f93c7336b
Sofia Vergara <ana.vergara@estudiantesunibague.edu.co>
2026-02-12 17:57:53 -0500

    Creacion clase ejemplar y otras cosas

---
+++ src/main/java/org/yourcompany/yourproject/Ejemplar.java
@@ -0,0 +1,17 @@
+package org.yourcompany.yourproject;
+
+public class Ejemplar {
+    private String codigo;
+    private Libro libro;
+
+    public Ejemplar(String codigo, Libro libro) {
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+}
--- src/main/java/org/yourcompany/yourproject/Libro.java
+++ src/main/java/org/yourcompany/yourproject/Libro.java
@@ -1,15 +1,23 @@
 package org.yourcompany.yourproject;
 
+import java.util.ArrayList;
+
 public class Libro {
     private String isbn;
     private String titulo;
+    private ArrayList<Ejemplar> ejemplares = new ArrayList<>();
 
-    public Libro(String isbn, String titulo) {
+    public Libro(String isbn, String titulo,ArrayList<Ejemplar> ejemplares) {
         this.isbn = isbn;
         this.titulo =titulo;
+        this.ejemplares = ejemplares;
     }
 
     public String getTitulo() {
         return titulo;
+    }
+
+    public Ejemplar crearEjemplar(String codigo){
+        Ejemplar ejemplar = new Ejemplar(codigo,this);
     }
 }
