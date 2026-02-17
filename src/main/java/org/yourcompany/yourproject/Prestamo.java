c84dfface66c3526a9d89c88f77e91197dee3f68
Paula C <paula.cubillos@estudiantesunibague.edu.co>
2026-02-17 10:44:34 -0500

    actualización

---
+++ src/main/java/org/yourcompany/yourproject/Prestamo.java
@@ -0,0 +1,13 @@
+
+package org.yourcompany.yourproject;
+
+import java.util.ArrayList;
+
+public class Prestamo {
+
+    private Socio socio;
+    private ArrayList<Ejemplar> ejemplares = new ArrayList<>();
+
+    
+
+}
