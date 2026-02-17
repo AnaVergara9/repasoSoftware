08fb103e62762ffe14d28a920192bdc59b6f69ae
Paula C <paula.cubillos@estudiantesunibague.edu.co>
2026-02-17 16:13:08 -0500

    Corrección prestamo

--- src/main/java/org/yourcompany/yourproject/Prestamo.java
+++ src/main/java/org/yourcompany/yourproject/Prestamo.java
@@ -8,6 +8,15 @@ public class Prestamo {
     private Socio socio;
     private ArrayList<Ejemplar> ejemplares = new ArrayList<>();
 
+    public Prestamo(Socio socio, ArrayList<Ejemplar> ejemplares) {
+        this.socio = socio;
+        this.ejemplares = ejemplares;
+    }
+
+    public void notificarCreacion(Notificador notificador){
+        String mensaje = "Se ha creado su préstamo con " + ejemplares.size() + " ejemplares.";
+        notificador.enviar(socio.getEmail(), mensaje);
+    }
     
 
 }
